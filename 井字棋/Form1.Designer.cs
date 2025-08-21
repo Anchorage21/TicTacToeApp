7c1833bcf2aebeeb63be97c656a4db44b4e9fa8e
Spa050816 <1605464814@qq.com>
2025-08-21 23:45:41 +0800

    Add files via upload

---
+++ 井字棋/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace TicTacToeWinForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnPos0 = new System.Windows.Forms.Button();
+            this.btnPos1 = new System.Windows.Forms.Button();
+            this.btnPos2 = new System.Windows.Forms.Button();
+            this.btnPos3 = new System.Windows.Forms.Button();
+            this.btnPos4 = new System.Windows.Forms.Button();
+            this.btnPos5 = new System.Windows.Forms.Button();
+            this.btnPos6 = new System.Windows.Forms.Button();
+            this.btnPos7 = new System.Windows.Forms.Button();
+            this.btnPos8 = new System.Windows.Forms.Button();
+            this.btnNewGamePlayerFirst = new System.Windows.Forms.Button();
+            this.btnNewGameComputerFirst = new System.Windows.Forms.Button();
+            this.btnShowHistory = new System.Windows.Forms.Button();
+            this.btnShowStats = new System.Windows.Forms.Button();
+            this.btnResetStats = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPos0
+            // 
+            this.btnPos0.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos0.Location = new System.Drawing.Point(30, 30);
+            this.btnPos0.Name = "btnPos0";
+            this.btnPos0.Size = new System.Drawing.Size(60, 60);
+            this.btnPos0.TabIndex = 0;
+            this.btnPos0.UseVisualStyleBackColor = true;
+            // 
+            // btnPos1
+            // 
+            this.btnPos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos1.Location = new System.Drawing.Point(96, 30);
+            this.btnPos1.Name = "btnPos1";
+            this.btnPos1.Size = new System.Drawing.Size(60, 60);
+            this.btnPos1.TabIndex = 1;
+            this.btnPos1.UseVisualStyleBackColor = true;
+            // 
+            // btnPos2
+            // 
+            this.btnPos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos2.Location = new System.Drawing.Point(162, 30);
+            this.btnPos2.Name = "btnPos2";
+            this.btnPos2.Size = new System.Drawing.Size(60, 60);
+            this.btnPos2.TabIndex = 2;
+            this.btnPos2.UseVisualStyleBackColor = true;
+            // 
+            // btnPos3
+            // 
+            this.btnPos3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos3.Location = new System.Drawing.Point(30, 96);
+            this.btnPos3.Name = "btnPos3";
+            this.btnPos3.Size = new System.Drawing.Size(60, 60);
+            this.btnPos3.TabIndex = 3;
+            this.btnPos3.UseVisualStyleBackColor = true;
+            // 
+            // btnPos4
+            // 
+            this.btnPos4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos4.Location = new System.Drawing.Point(96, 96);
+            this.btnPos4.Name = "btnPos4";
+            this.btnPos4.Size = new System.Drawing.Size(60, 60);
+            this.btnPos4.TabIndex = 4;
+            this.btnPos4.UseVisualStyleBackColor = true;
+            // 
+            // btnPos5
+            // 
+            this.btnPos5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos5.Location = new System.Drawing.Point(162, 96);
+            this.btnPos5.Name = "btnPos5";
+            this.btnPos5.Size = new System.Drawing.Size(60, 60);
+            this.btnPos5.TabIndex = 5;
+            this.btnPos5.UseVisualStyleBackColor = true;
+            // 
+            // btnPos6
+            // 
+            this.btnPos6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos6.Location = new System.Drawing.Point(30, 162);
+            this.btnPos6.Name = "btnPos6";
+            this.btnPos6.Size = new System.Drawing.Size(60, 60);
+            this.btnPos6.TabIndex = 6;
+            this.btnPos6.UseVisualStyleBackColor = true;
+            // 
+            // btnPos7
+            // 
+            this.btnPos7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos7.Location = new System.Drawing.Point(96, 162);
+            this.btnPos7.Name = "btnPos7";
+            this.btnPos7.Size = new System.Drawing.Size(60, 60);
+            this.btnPos7.TabIndex = 7;
+            this.btnPos7.UseVisualStyleBackColor = true;
+            // 
+            // btnPos8
+            // 
+            this.btnPos8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPos8.Location = new System.Drawing.Point(162, 162);
+            this.btnPos8.Name = "btnPos8";
+            this.btnPos8.Size = new System.Drawing.Size(60, 60);
+            this.btnPos8.TabIndex = 8;
+            this.btnPos8.UseVisualStyleBackColor = true;
+            // 
+            // btnNewGamePlayerFirst
+            // 
+            this.btnNewGamePlayerFirst.Location = new System.Drawing.Point(250, 30);
+            this.btnNewGamePlayerFirst.Name = "btnNewGamePlayerFirst";
+            this.btnNewGamePlayerFirst.Size = new System.Drawing.Size(150, 30);
+            this.btnNewGamePlayerFirst.TabIndex = 9;
+            this.btnNewGamePlayerFirst.Text = "新游戏 (玩家先手)";
+            this.btnNewGamePlayerFirst.UseVisualStyleBackColor = true;
+            this.btnNewGamePlayerFirst.Click += new System.EventHandler(this.btnNewGamePlayerFirst_Click);
+            // 
+            // btnNewGameComputerFirst
+            // 
+            this.btnNewGameComputerFirst.Location = new System.Drawing.Point(250, 66);
+            this.btnNewGameComputerFirst.Name = "btnNewGameComputerFirst";
+            this.btnNewGameComputerFirst.Size = new System.Drawing.Size(150, 30);
+            this.btnNewGameComputerFirst.TabIndex = 10;
+            this.btnNewGameComputerFirst.Text = "新游戏 (电脑先手)";
+            this.btnNewGameComputerFirst.UseVisualStyleBackColor = true;
+            this.btnNewGameComputerFirst.Click += new System.EventHandler(this.btnNewGameComputerFirst_Click);
+            // 
+            // btnShowHistory
+            // 
+            this.btnShowHistory.Location = new System.Drawing.Point(250, 102);
+            this.btnShowHistory.Name = "btnShowHistory";
+            this.btnShowHistory.Size = new System.Drawing.Size(150, 30);
+            this.btnShowHistory.TabIndex = 11;
+            this.btnShowHistory.Text = "显示游戏历史";
+            this.btnShowHistory.UseVisualStyleBackColor = true;
+            this.btnShowHistory.Click += new System.EventHandler(this.btnShowHistory_Click);
+            // 
+            // btnShowStats
+            // 
+            this.btnShowStats.Location = new System.Drawing.Point(250, 138);
+            this.btnShowStats.Name = "btnShowStats";
+            this.btnShowStats.Size = new System.Drawing.Size(150, 30);
+            this.btnShowStats.TabIndex = 12;
+            this.btnShowStats.Text = "显示统计";
+            this.btnShowStats.UseVisualStyleBackColor = true;
+            this.btnShowStats.Click += new System.EventHandler(this.btnShowStats_Click);
+            // 
+            // btnResetStats
+            // 
+            this.btnResetStats.Location = new System.Drawing.Point(250, 174);
+            this.btnResetStats.Name = "btnResetStats";
+            this.btnResetStats.Size = new System.Drawing.Size(150, 30);
+            this.btnResetStats.TabIndex = 13;
+            this.btnResetStats.Text = "重置统计";
+            this.btnResetStats.UseVisualStyleBackColor = true;
+            this.btnResetStats.Click += new System.EventHandler(this.btnResetStats_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(250, 210);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 30);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(30, 240);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(55, 13);
+            this.lblStatus.TabIndex = 15;
+            this.lblStatus.Text = "游戏状态";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 271);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnResetStats);
+            this.Controls.Add(this.btnShowStats);
+            this.Controls.Add(this.btnShowHistory);
+            this.Controls.Add(this.btnNewGameComputerFirst);
+            this.Controls.Add(this.btnNewGamePlayerFirst);
+            this.Controls.Add(this.btnPos8);
+            this.Controls.Add(this.btnPos7);
+            this.Controls.Add(this.btnPos6);
+            this.Controls.Add(this.btnPos5);
+            this.Controls.Add(this.btnPos4);
+            this.Controls.Add(this.btnPos3);
+            this.Controls.Add(this.btnPos2);
+            this.Controls.Add(this.btnPos1);
+            this.Controls.Add(this.btnPos0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "井字棋游戏";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPos0;
+        private System.Windows.Forms.Button btnPos1;
+        private System.Windows.Forms.Button btnPos2;
+        private System.Windows.Forms.Button btnPos3;
+        private System.Windows.Forms.Button btnPos4;
+        private System.Windows.Forms.Button btnPos5;
+        private System.Windows.Forms.Button btnPos6;
+        private System.Windows.Forms.Button btnPos7;
+        private System.Windows.Forms.Button btnPos8;
+        private System.Windows.Forms.Button btnNewGamePlayerFirst;
+        private System.Windows.Forms.Button btnNewGameComputerFirst;
+        private System.Windows.Forms.Button btnShowHistory;
+        private System.Windows.Forms.Button btnShowStats;
+        private System.Windows.Forms.Button btnResetStats;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ 井字棋/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TicTacToeWinForms
+{
+    public partial class Form1 : Form
+    {
+        private char[] board;
+        private char humanSymbol;
+        private char computerSymbol;
+        private bool humanFirst;
+        private bool gameOver;
+        private int humanWins;
+        private int computerWins;
+        private int draws;
+        private List<string> gameHistory;
+
+        private Button[] boardButtons;
+        private const string STATS_FILE = "game_stats.txt";
+        private const string HISTORY_FILE = "game_history.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+            LoadStatistics();
+            LoadGameHistory();
+        }
+
+        private void InitializeGame()
+        {
+            board = new char[9];
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+            gameOver = true;
+            gameHistory = new List<string>();
+
+            // 关联棋盘按钮
+            boardButtons = new Button[]
+            {
+                btnPos0, btnPos1, btnPos2,
+                btnPos3, btnPos4, btnPos5,
+                btnPos6, btnPos7, btnPos8
+            };
+
+            // 设置按钮事件
+            for (int i = 0; i < boardButtons.Length; i++)
+            {
+                int index = i; // 重要：创建局部变量捕获
+                boardButtons[i].Click += (s, e) => BoardButtonClick(index);
+            }
+
+            UpdateStatusLabel();
+        }
+
+        private void BoardButtonClick(int position)
+        {
+            if (gameOver || board[position] != '\0')
+                return;
+
+            // 玩家移动
+            MakeMove(position, humanSymbol);
+            boardButtons[position].Text = humanSymbol.ToString();
+            boardButtons[position].Enabled = false;
+
+            if (CheckWin(humanSymbol))
+            {
+                gameOver = true;
+                humanWins++;
+                gameHistory.Add($"{DateTime.Now}: 玩家获胜");
+                MessageBox.Show("恭喜！你赢了！", "游戏结束", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                UpdateStatusLabel();
+                SaveGameHistory();
+                SaveStatistics();
+                return;
+            }
+
+            if (CheckDraw())
+            {
+                gameOver = true;
+                draws++;
+                gameHistory.Add($"{DateTime.Now}: 平局");
+                MessageBox.Show("平局！", "游戏结束", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                UpdateStatusLabel();
+                SaveGameHistory();
+                SaveStatistics();
+                return;
+            }
+
+            // 电脑移动
+            ComputerMove();
+        }
+
+        private void ComputerMove()
+        {
+            if (gameOver) return;
+
+            // 简单的AI：先尝试赢，再阻止玩家赢，否则随机
+            int move = -1;
+
+            // 尝试赢
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == '\0')
+                {
+                    board[i] = computerSymbol;
+                    if (CheckWin(computerSymbol))
+                    {
+                        move = i;
+                        board[i] = '\0'; // 撤销测试移动
+                        break;
+                    }
+                    board[i] = '\0'; // 撤销测试移动
+                }
+            }
+
+            // 阻止玩家赢
+            if (move == -1)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i] == '\0')
+                    {
+                        board[i] = humanSymbol;
+                        if (CheckWin(humanSymbol))
+                        {
+                            move = i;
+                            board[i] = '\0'; // 撤销测试移动
+                            break;
+                        }
+                        board[i] = '\0'; // 撤销测试移动
+                    }
+                }
+            }
+
+            // 随机移动
+            if (move == -1)
+            {
+                Random rand = new Random();
+                List<int> emptyPositions = new List<int>();
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i] == '\0')
+                        emptyPositions.Add(i);
+                }
+
+                if (emptyPositions.Count > 0)
+                    move = emptyPositions[rand.Next(emptyPositions.Count)];
+            }
+
+            if (move != -1)
+            {
+                MakeMove(move, computerSymbol);
+                boardButtons[move].Text = computerSymbol.ToString();
+                boardButtons[move].Enabled = false;
+
+                if (CheckWin(computerSymbol))
+                {
+                    gameOver = true;
+                    computerWins++;
+                    gameHistory.Add($"{DateTime.Now}: 电脑获胜");
+                    MessageBox.Show("电脑赢了！", "游戏结束", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    UpdateStatusLabel();
+                    SaveGameHistory();
+                    SaveStatistics();
+                }
+                else if (CheckDraw())
+                {
+                    gameOver = true;
+                    draws++;
+                    gameHistory.Add($"{DateTime.Now}: 平局");
+                    MessageBox.Show("平局！", "游戏结束", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    UpdateStatusLabel();
+                    SaveGameHistory();
+                    SaveStatistics();
+                }
+            }
+        }
+
+        private void MakeMove(int position, char symbol)
+        {
+            board[position] = symbol;
+        }
+
+        private bool CheckWin(char symbol)
+        {
+            // 检查行
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i * 3] == symbol && board[i * 3 + 1] == symbol && board[i * 3 + 2] == symbol)
+                    return true;
+            }
+
+            // 检查列
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i] == symbol && board[i + 3] == symbol && board[i + 6] == symbol)
+                    return true;
+            }
+
+            // 检查对角线
+            if (board[0] == symbol && board[4] == symbol && board[8] == symbol)
+                return true;
+
+            if (board[2] == symbol && board[4] == symbol && board[6] == symbol)
+                return true;
+
+            return false;
+        }
+
+        private bool CheckDraw()
+        {
+            return board.All(cell => cell != '\0');
+        }
+
+        private void NewGame(bool humanFirst)
+        {
+            // 重置棋盘
+            for (int i = 0; i < 9; i++)
+            {
+                board[i] = '\0';
+                boardButtons[i].Text = "";
+                boardButtons[i].Enabled = true;
+            }
+
+            this.humanFirst = humanFirst;
+            gameOver = false;
+
+            if (!humanFirst)
+            {
+                ComputerMove();
+            }
+
+            UpdateStatusLabel();
+        }
+
+        private void UpdateStatusLabel()
+        {
+            lblStatus.Text = $"玩家: {humanWins} 胜 | 电脑: {computerWins} 胜 | 平局: {draws}";
+        }
+
+        private void SaveStatistics()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(STATS_FILE))
+                {
+                    writer.WriteLine($"HumanWins:{humanWins}");
+                    writer.WriteLine($"ComputerWins:{computerWins}");
+                    writer.WriteLine($"Draws:{draws}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"保存统计数据时出错: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadStatistics()
+        {
+            if (File.Exists(STATS_FILE))
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(STATS_FILE);
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(':');
+                        if (parts.Length == 2)
+                        {
+                            switch (parts[0])
+                            {
+                                case "HumanWins":
+                                    int.TryParse(parts[1], out humanWins);
+                                    break;
+                                case "ComputerWins":
+                                    int.TryParse(parts[1], out computerWins);
+                                    break;
+                                case "Draws":
+                                    int.TryParse(parts[1], out draws);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"加载统计数据时出错: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void SaveGameHistory()
+        {
+            try
+            {
+                File.WriteAllLines(HISTORY_FILE, gameHistory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"保存游戏历史时出错: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadGameHistory()
+        {
+            if (File.Exists(HISTORY_FILE))
+            {
+                try
+                {
+                    gameHistory = new List<string>(File.ReadAllLines(HISTORY_FILE));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"加载游戏历史时出错: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnNewGamePlayerFirst_Click(object sender, EventArgs e)
+        {
+            NewGame(true);
+        }
+
+        private void btnNewGameComputerFirst_Click(object sender, EventArgs e)
+        {
+            NewGame(false);
+        }
+
+        private void btnShowHistory_Click(object sender, EventArgs e)
+        {
+            string history = gameHistory.Count > 0 ?
+                string.Join(Environment.NewLine, gameHistory) :
+                "暂无游戏记录";
+
+            MessageBox.Show(history, "游戏历史", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnShowStats_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                $"玩家获胜: {humanWins} 次\n电脑获胜: {computerWins} 次\n平局: {draws} 次",
+                "游戏统计",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void btnResetStats_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "确定要重置所有统计数据吗？",
+                "确认重置",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                humanWins = 0;
+                computerWins = 0;
+                draws = 0;
+                gameHistory.Clear();
+
+                try
+                {
+                    if (File.Exists(STATS_FILE))
+                        File.Delete(STATS_FILE);
+                    if (File.Exists(HISTORY_FILE))
+                        File.Delete(HISTORY_FILE);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"删除统计文件时出错: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                UpdateStatusLabel();
+                MessageBox.Show("统计数据已重置", "信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            SaveStatistics();
+            SaveGameHistory();
+            Application.Exit();
+        }
+    }
+}
